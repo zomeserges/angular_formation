18ba1d1734ad1d16bc3ea03407bdb8c59c98570e
zomeserges <zomeserges@gmail.com>
2022-04-16 09:11:28 +0200

    comprendre les composants angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component(
+  {
+    selector: 'app-root',
+    template:`<h1>{{title}}</h1>`,
+    styleUrls:[]
+  }
+)
+export class AppComponent{
+  title: string = "ZMS-TECH";
+}
