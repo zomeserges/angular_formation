4bfdc89b719aa5c6d476f15f7277bf9d70d49077
zomeserges <zomeserges@gmail.com>
2022-04-17 09:10:05 +0200

    using of pipe like currency and uppercase pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
+import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import localeFr from '@angular/common/locales/fr';
 import { AppComponent } from './app.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 
+registerLocaleData(localeFr,"fr");
 @NgModule({
   declarations: [
     AppComponent,
