caab9e39839633de67e99cf6476f6ed16e0bf811
zomeserges <zomeserges@gmail.com>
2022-04-18 09:07:18 +0200

    adding navbar and router outlet to navigage in different pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { registerLocaleData } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import localeFr from '@angular/common/locales/fr';
+
 import { AppComponent } from './app.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { ReplaceComma } from './shered/pipes/replace-comma.pipe';
 import { starRatingComponent } from './shered/components/star-rating/star-rating.component';
+import { HomeComponent } from './home/home.component';
+import { HotelDetailComponent } from './hotel-list/hotel-detail/hotel-detail.component';
 
 registerLocaleData(localeFr,"fr");
 @NgModule({
@@ -15,12 +19,21 @@ registerLocaleData(localeFr,"fr");
     AppComponent,
     HotelListComponent,
     ReplaceComma,
-    starRatingComponent
+    starRatingComponent,
+    HomeComponent,
+    HotelDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'hotel/:id', component: HotelDetailComponent},
+      {path: 'hotels', component: HotelListComponent},
+      {path: '**', redirectTo: 'home', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
