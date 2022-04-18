f2e44ba1808da0b9b9350b172cf7081264197adf
zomeserges <zomeserges@gmail.com>
2022-04-18 10:36:15 +0200

    reading of url parameters and building hotel-detail-component

--- src/app/hotel-list/hotel-detail/hotel-detail.component.ts
+++ src/app/hotel-list/hotel-detail/hotel-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IHotel } from '../hotel';
+import { HotelListService } from '../hotel-list.service';
 
 @Component({
   selector: 'app-hotel-detail',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelDetailComponent implements OnInit {
 
-  constructor() { }
+  public hotel: IHotel = <IHotel>{};
+  constructor(
+    private route: ActivatedRoute,
+    private hotelService: HotelListService
+    ) { }
 
   ngOnInit(): void {
+
+    if(!!this.route.snapshot.paramMap.get('id')){
+      let id: number = parseInt(''+this.route.snapshot.paramMap.get('id'),10)
+      this.hotelService.getHotels().subscribe((hotels: IHotel[]) =>{
+        this.hotel = <IHotel>hotels.find(hotel => hotel.hotelId === id)
+        console.log('my hotel: ', this.hotel)
+      })
+
+    }
+
+
   }
 
 }
